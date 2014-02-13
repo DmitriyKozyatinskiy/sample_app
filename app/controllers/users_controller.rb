a41f169925f498850923e46a390cf8c7092a179c
Dima <dmitriy.kozyatinskiy@gmail.com>
2014-02-13 17:46:13 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end                                  :password_confirmation)
     end  
 end
